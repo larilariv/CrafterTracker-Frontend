4a81cf7676ea2ddd50571ad2b058eae19c14380a
Larissa Vieira <lv23@nyu.edu>
2022-07-18 18:55:05 -0400

    All project components imported and exported through index.js

--- src/components/project/index.js
+++ src/components/project/index.js
@@ -1,3 +1,15 @@
+import CreateProject from "./CreateProject";
+import EditProject from "./EditProject";
+import ProjectDetail from "./ProjectDetail";
 import ProjectFilter from "./ProjectFilter";
+import ProjectList from "./ProjectList";
+import ProjectCard from "./ProjectCard";
 
-export default ProjectFilter;
+export {
+  CreateProject,
+  EditProject,
+  ProjectDetail,
+  ProjectFilter,
+  ProjectList,
+  ProjectCard,
+};
